1f6cda82edd806d57af8e4aeeca8b111689a7e76
roromusic <roromusic@ucla.edu>
2018-01-25 18:25:42 -0800

    clean variables and functions

--- app/app.js
+++ app/app.js
@@ -1,31 +1,30 @@
-const sound = "https://freesound.org/data/previews/34/34272_304419-lq.mp3";
-const kick = "https://freesound.org/data/previews/132/132584_2409787-lq.mp3";
-const snare = "https://freesound.org/data/previews/13/13750_32468-lq.mp3";
+const cowbellFile = "https://freesound.org/data/previews/34/34272_304419-lq.mp3";
+const kickFile = "https://freesound.org/data/previews/132/132584_2409787-lq.mp3";
+const snareFile = "https://freesound.org/data/previews/13/13750_32468-lq.mp3";
 
 let context = new (window.AudioContext || window.webkitAudioContext)();
-let cowbell,
-    gainNode,
-    source;
 
-function getBuffer() {
-  let request = new XMLHttpRequest();
-  request.open('get', sound, true);
-  request.responseType = 'arraybuffer';
-  request.onload = () => {
-    context.decodeAudioData(request.response, (buffer) => {
-      cowbell = buffer;
-      setup();
-      console.log("buffer complete", buffer);
-    });
-  };
-  request.send();
+let instruments = new Map([["cowbell"]]);
+let gainNode,
+    source,
+    metronomeOn = true;
+
+async function getBuffer(file, instrument) {
+  const promise = await fetch(file);
+  const data = await promise.arrayBuffer();
+  const buffer = await context.decodeAudioData(data).then(function(decodedData){
+    return decodedData
+  });
+
+  instruments.set(instrument, buffer);
+  
 }
-getBuffer();
+getBuffer(cowbellFile, "cowbell");
 
-function setup() {
+function setup(instrument) {
    gainNode = context.createGain();
    source = context.createBufferSource();
-   source.buffer = cowbell;
+   source.buffer = instruments.get(instrument);
    source.connect(gainNode);
    gainNode.connect(context.destination);
    gainNode.gain.setValueAtTime(0.8, context.currentTime);
@@ -34,8 +33,8 @@ function setup() {
 }
    
 
-function play(time, bpm) {
-  setup();
+function loopMetronome(time, bpm, instrument) {
+  setup(instrument);
   source.start(time);
   source.loopEnd = bpm;
 }
@@ -52,7 +51,9 @@ let bpm = document.querySelector("input");
 
 playBtn.addEventListener("mousedown", function() {
   const newBPM = (60 / bpm.value).toFixed(2);
-  play(context.currentTime, newBPM);
+  if(metronomeOn){
+    loopMetronome(context.currentTime, newBPM, "cowbell");
+  }
 })
 
 stopBtn.addEventListener("click", function() {
