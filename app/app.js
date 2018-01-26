ca97ee4e9de57be70b4b3071e060a9211d1b871c
roromusic <roromusic@ucla.edu>
2018-01-25 23:01:34 -0800

    add repeating beats

--- app/app.js
+++ app/app.js
@@ -5,7 +5,11 @@ const snareFile = "https://freesound.org/data/previews/13/13750_32468-lq.mp3";
 let context = new (window.AudioContext || window.webkitAudioContext)();
 
 let instruments = new Map([["cowbell"], ["kick"], ["snare"]]);
-let source = {};
+let source = {
+  cowbell: [],
+  kick: [],
+  snare: []
+};
 let scheduledPlays = [];
 let hits = {
   kick: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
@@ -15,7 +19,9 @@ let hits = {
 let gainNode,
     bpm = 1.00,
     sixteenth = .25,
-    metronomeOn = false;
+    nextRepeatPoint,
+    metronomeOn = false,
+    playing = false;
 
 async function getBuffer(file, instrument) {
   const promise = await fetch(file);
@@ -33,46 +39,58 @@ getBuffer(snareFile, "snare");
 
 function setup(instrument) {
    gainNode = context.createGain();
-   source[instrument] = context.createBufferSource();
-   source[instrument].buffer = instruments.get(instrument);
-   source[instrument].connect(gainNode);
+   let sourceNode = context.createBufferSource();
+   sourceNode.buffer = instruments.get(instrument);
+   sourceNode.connect(gainNode);
+   source[instrument].push(sourceNode);
+   sourceNode.onended = function(event) {
+     source[instrument].shift();
+     if(playing === true && source[instrument].length === 0){
+       playBeat()
+     }
+   }
    gainNode.connect(context.destination);
    gainNode.gain.setValueAtTime(0.8, context.currentTime);
-}
-   
+
+   return sourceNode;
+}  
 
 function loopMetronome(time, instrument) {
-  setup(instrument);
-  source[instrument].start(time);
-  source[instrument].loop = true;
-  source[instrument].loopEnd = bpm;
+  let sourceNode = setup(instrument);
+  sourceNode.start(time);
+  sourceNode.loop = true;
+  sourceNode.loopEnd = bpm;
 }
 
 function playSample(time, instrument) {
-  setup(instrument);
-  source[instrument].start(time);
+  let sourceNode = setup(instrument);
+  sourceNode.start(time);
 }
 
 function playBeat() {
+  const currentTime = nextRepeatPoint === undefined ? context.currentTime : nextRepeatPoint;
+
   hits.kick.forEach((partial, index) => {
     if(partial) {
-      playSample(context.currentTime + sixteenth * index, "kick")
+      playSample(currentTime + sixteenth * index, "kick")
     }
     
   })
 
   hits.snare.forEach((partial, index) => {
     if(partial) {
-      playSample(context.currentTime + sixteenth * index, "snare")
+      playSample(currentTime + sixteenth * index, "snare")
     }
     
   })
+
+  nextRepeatPoint = currentTime + bpm * 4;
 }
 
 function stop() {
   var ct = context.currentTime + 0.1;
   gainNode.gain.exponentialRampToValueAtTime(0.001, ct);
-  source["cowbell"].stop(ct);
+  if (source["cowbell"][0]) source["cowbell"][0].stop(ct);
 }
 
 let playBtn = document.querySelector("#play");
@@ -80,7 +98,7 @@ let stopBtn = document.querySelector("#stop");
 let bpmInput = document.querySelector("input");
 
 playBtn.addEventListener("mousedown", function() {
-
+  playing = true;
   if(metronomeOn){
     loopMetronome(context.currentTime, "cowbell");
   }else {
