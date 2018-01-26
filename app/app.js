5f92ca99c1aee34814c1231e837c52e1b270496c
roromusic <roromusic@ucla.edu>
2018-01-25 21:02:18 -0800

    add playBeat function

--- app/app.js
+++ app/app.js
@@ -7,11 +7,15 @@ let context = new (window.AudioContext || window.webkitAudioContext)();
 let instruments = new Map([["cowbell"], ["kick"], ["snare"]]);
 let source = {};
 let scheduledPlays = [];
+let hits = {
+  kick: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+  snare: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+};
 
 let gainNode,
     bpm = 1.00,
     sixteenth = .25,
-    metronomeOn = true;
+    metronomeOn = false;
 
 async function getBuffer(file, instrument) {
   const promise = await fetch(file);
@@ -49,6 +53,22 @@ function playSample(time, instrument) {
   source[instrument].start(time);
 }
 
+function playBeat() {
+  hits.kick.forEach((partial, index) => {
+    if(partial) {
+      playSample(context.currentTime + sixteenth * index, "kick")
+    }
+    
+  })
+
+  hits.snare.forEach((partial, index) => {
+    if(partial) {
+      playSample(context.currentTime + sixteenth * index, "snare")
+    }
+    
+  })
+}
+
 function stop() {
   var ct = context.currentTime + 0.1;
   gainNode.gain.exponentialRampToValueAtTime(0.001, ct);
@@ -63,6 +83,8 @@ playBtn.addEventListener("mousedown", function() {
 
   if(metronomeOn){
     loopMetronome(context.currentTime, "cowbell");
+  }else {
+    playBeat();
   }
 })
 
@@ -72,7 +94,7 @@ stopBtn.addEventListener("click", function() {
 
 bpmInput.addEventListener("change", function(e) {
   bpm = (60 / e.target.value).toFixed(2);
-  partial = (bpm / 4).toFixed(2);
+  sixteenth = (bpm / 4).toFixed(2);
   console.log(bpm);
   if(source["cowbell"]){
     source["cowbell"].loopEnd = bpm;
@@ -81,8 +103,13 @@ bpmInput.addEventListener("change", function(e) {
 })
 
 document.querySelector("#instruments").addEventListener("click", (e) => {
-  if(e.target.className === "note"){
-    console.log(e.target.parentNode.parentNode.id)
-    playSample(context.currentTime, e.target.parentNode.parentNode.id);
+  if(e.target.classList.contains("note")){
+    const instrument = e.target.parentNode.parentNode.id;
+    const partial = e.target.getAttribute("data-partial");
+    
+    e.target.classList.toggle("note-play");
+    hits[instrument][partial] = !hits[instrument][partial];
+    console.log(hits[instrument][partial])
+    playSample(context.currentTime, instrument);
   }
 })
